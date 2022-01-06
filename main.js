ba653393b68ec6b6b2ce5a8d419239ccd611396d
Martha Mendoza <71796360+Mtc-21@users.noreply.github.com>
2022-01-06 19:57:12 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,21 @@
         e.target.setAttribute("src", "images/icon-hamburger.svg") :
         e.target.setAttribute("src", "images/icon-close.svg")
     });
+    // animation scroll
+    const elements=document.querySelectorAll(".transition");
+   
+    const ob=new IntersectionObserver(entries=>{
+      entries.forEach(entry => {
+        entry.target.classList.toggle("visible", entry.isIntersecting);
+        if(entry.isIntersecting) ob.unobserve(entry.target)
+      },{
+        root:null,
+        rootMargin:"-50px -50px 0px -50px",
+        threshold: 1.0,
+      });
+    }
+    );
+  
+    elements.forEach(elem => {
+        ob.observe(elem)
+    });
